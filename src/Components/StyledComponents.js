f9c1fd886cba91e7314a3a255c93066e0fe03ca5
David Pérez <deividpa022@gmail.com>
2022-11-03 13:41:32 -0500

    Se emplea el uso de Componentes de la librería style-components como lo son: keyframes para animaciones, createGlobalStyle para afectar toda las etiquetas del renderizado, y ThemeProvider para crear un diseño basado en plantillas

--- src/Components/StyledComponents.js
+++ src/Components/StyledComponents.js
@@ -1,11 +1,26 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled, {
+  css,
+  keyframes,
+  ThemeProvider,
+  createGlobalStyle,
+} from "styled-components";
 
 export default function StyledComponents() {
   let mainColor = "#db7093",
     mainAlphaColor80 = "#db709380";
 
   const setTransitionTime = (time) => `all ${time}s ease-in-out`;
+
+  const fadeIn = keyframes`
+    0% {
+      opacity:0;
+    }
+
+    100% {
+      opacity:1;
+    }
+  `;
 
   const MyH3 = styled.h3`
     background-color: ${mainColor};
@@ -14,6 +29,7 @@ export default function StyledComponents() {
     padding: 5px;
     text-align: center;
     transition: ${setTransitionTime(0.5)};
+    animation: ${fadeIn} 2s ease-out;
 
     ${(props) =>
       props.isButton &&
@@ -29,8 +45,42 @@ export default function StyledComponents() {
     }
   `;
 
+  // Utilización del módulo «Theme Provider» de styled-components
+
+  const light = {
+    color: "#222",
+    bgColor: "#DDD",
+  };
+
+  const dark = {
+    color: "#DDD",
+    bgColor: "#222",
+  };
+
+  const Box = styled.div`
+    padding: 1rem;
+    margin: 1rem;
+    color: ${({ theme }) => theme.color};
+    background-color: ${({ theme }) => theme.bgColor};
+  `;
+
+  const BoxRounded = styled(Box)`
+    border-radius: 1rem;
+    margin: 0 auto;
+    max-width: 80%;
+  `;
+
+  const GlobalStyle = createGlobalStyle`
+    h2 {
+      background-color: #0FA;
+      color: #000;
+      text-transform: uppercase;
+    } 
+  `;
+
   return (
     <>
+      <GlobalStyle />
       <h2>Componentes Estilizados con styled-components library</h2>
       <MyH3 color="#333">
         Soy un H3 estilizado con la lib. styled-components
@@ -40,6 +90,18 @@ export default function StyledComponents() {
       </MyH3>
       <MyH3>Soy un H3 estilizado con la lib. styled-components</MyH3>
       <MyH3 isButton>Soy un H3 estilizado como Botón</MyH3>
+      <ThemeProvider theme={light}>
+        <Box>Soy una caja light</Box>
+        <BoxRounded theme={light}>
+          Soy una caja con theme, que hereda de Box y adiciona borde redondeado
+        </BoxRounded>
+      </ThemeProvider>
+      <ThemeProvider theme={dark}>
+        <Box>Soy una caja dark</Box>
+        <BoxRounded theme={dark}>
+          Soy una caja con theme, que hereda de Box y adiciona borde redondeado
+        </BoxRounded>
+      </ThemeProvider>
     </>
   );
 }
